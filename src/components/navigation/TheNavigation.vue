64b633e3340c7ee9ebffe4011a229b69af63803a
lgiesen <leo.richard.giesen@gmail.com>
2021-03-10 00:00:14 +0100

    :fire: removing redundant code

--- src/components/navigation/TheNavigation.vue
+++ src/components/navigation/TheNavigation.vue
@@ -14,35 +14,9 @@ v-app-bar(app)
     v-tab(
       v-for="view in views" :key="view.to.name"
       :to="view.to"
-      ) {{ view.tag }}  
+      ) {{ view.tag }}
   v-btn(icon) 
     v-icon(@click="toggleTheme") mdi-brightness-6
-//- v-card#nav
-  template
-    div.overflow-hidden
-      v-app-bar(
-        absolute src='https://picsum.photos/1920/1080?random' color='#6A76AB' dark='' 
-        shrink-on-scroll='' prominent=''  fade-img-on-scroll='' scroll-target='#scrolling-techniques-3')
-        template(v-slot:img='{ props }')
-          v-img(v-bind='props' 
-            gradient='to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)')
-        v-app-bar-nav-icon
-        v-app-bar-title Portfolio - Leo Giesen
-        v-spacer
-        //- socials
-        div
-          v-btn(icon='')
-            v-icon mdi-magnify
-          v-btn(icon='')
-            v-icon mdi-heart
-          v-btn(icon='')
-            v-icon(@click="toggleTheme") mdi-brightness-6
-        template(v-slot:extension='')
-          v-tabs(align-with-title='')
-            v-tab(
-              v-for="view in views" :key="view.to.name"
-              :to="view.to"
-              ) {{ view.tag }}   
 </template>
 
 <script>
