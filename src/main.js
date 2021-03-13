ad1733bbe9fce01c131b307991fcef8eba5ccd4a
lgiesen <leo.richard.giesen@gmail.com>
2021-03-13 15:20:00 +0100

    smaller changes

--- src/main.js
+++ src/main.js
@@ -2,13 +2,17 @@ import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import router from "./router/router.js";
+import store from "./store";
 // import store from "./store/store.js";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+
   // store,
   vuetify,
+
+  store,
   render: h => h(App)
 }).$mount("#app");
---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import Vuetify from "@/plugins/vuetify";
+import Vue from "vue";
+import Vuex from "vuex";
+import auth from "./modules/auth.js";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    showContactDialog: false
+  },
+  mutations: {},
+  actions: {},
+  getters: {
+    isDarkTheme() {
+      return Vuetify.framework.theme.dark;
+    }
+  },
+  modules: {
+    auth
+  }
+});
