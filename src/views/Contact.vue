81a3a4ac9516d929141b96856e6d9ea7f1f411b5
lgiesen <leo.richard.giesen@gmail.com>
2021-03-14 16:06:48 +0100

    :lipstick: background image of contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,75 +1,71 @@
 <template lang="pug">
 v-container.ma-12.mx-auto(persistant width="80vw" height=" 80vh")
   v-card
-    v-card-title
-      h2.display-1 Contact Me
-    v-card-subtitle 
-      h4.primary--text Subtitle
-    v-card-text
-      v-form#form(
-        ref="form"
-        @submit.prevent="submit")
-        v-row
-          v-col
-            v-text-field#name(
-              name="name"
-              required
-              v-model="name"
-              :counter="20"
-              :error-messages="nameErrors"
-              @input="$v.name.$touch()"
-              @blur="$v.name.$touch()"
-              label="Name*"
-              prepend-icon="mdi-account")
-          v-col
-            v-text-field#email(
-              name="email"
-              required
-              v-model="email"
-              :error-messages="emailErrors"
-              @input="$v.email.$touch()"
-              @blur="$v.email.$touch()"
-              label="Email*" 
-              prepend-icon="mdi-email")
-        v-row
-          v-col
-            v-text-field#subject(
-              name="subject"
-              required
-              v-model="subject"
-              :error-messages="subjectErrors"
-              @input="$v.subject.$touch()"
-              @blur="$v.subject.$touch()"
-              label="Subject*" 
-              prepend-icon="mdi-message")
-        v-row 
-          v-col 
-            v-textarea#message(
-              name="message"
-              required
-              v-model="message"
-              :error-messages="messageErrors"
-              @input="$v.message.$touch()"
-              @blur="$v.message.$touch()"
-              label="Message*" 
-              prepend-icon="mdi-text")
-        v-divider
-        v-card-actions
-          v-btn(
-            color="primary"
-            @click="clear")
-            v-icon mdi-close-circle-outline
-            | Clear
-          v-spacer
-          v-btn(:disabled="!this.isMounted")
-            v-icon mdi-send
-            input#button(
-              type="submit"
-              color="secondary"
-              value="Send Email") 
-        
-      p Dropdown: job offer, interaction, other &rarr; user may choose
-      p modern background: blurred image
+    v-img(:src="imageURL[0].url"
+      gradient="rgba(255,255,255,.8), rgba(255,255,255,.8)")
+      div.foregroundCard
+        v-card-title
+          h2.display-1 Contact Me
+        v-card-subtitle 
+          h4 I'd be happy to hear from you! You can also contact me via 
+            a(href="mailto:leo.richard.giesen@gmail.com") mail
+            | .
+        v-card-text
+          v-form#form(ref="form"
+            @submit.prevent="submit")
+            v-row
+              v-col
+                v-text-field#name(required
+                  name="name" label="Name*"
+                  v-model="name" 
+                  :counter="20"
+                  :error-messages="nameErrors"
+                  @input="$v.name.$touch()" @blur="$v.name.$touch()"
+                  prepend-icon="mdi-account")
+              v-col
+                v-text-field#email(required
+                  name="email" label="Email*" 
+                  v-model="email"
+                  :error-messages="emailErrors"
+                  @input="$v.email.$touch()" @blur="$v.email.$touch()"
+                  prepend-icon="mdi-email")
+            v-row
+              v-col
+                v-text-field#subject(required
+                  name="subject" label="Subject*" 
+                  v-model="subject"
+                  :error-messages="subjectErrors"
+                  @input="$v.subject.$touch()" @blur="$v.subject.$touch()"
+                  prepend-icon="mdi-message")
+            v-row 
+              v-col 
+                v-textarea#message(required
+                  name="message" label="Message*" 
+                  v-model="message"
+                  :error-messages="messageErrors"
+                  @input="$v.message.$touch()" @blur="$v.message.$touch()"
+                  prepend-icon="mdi-text")
+            v-divider
+            v-card-actions
+              v-btn(
+                color="primary"
+                @click="clear")
+                v-icon.mr-2 mdi-close-circle-outline
+                | Clear
+              v-spacer
+              v-btn(color="secondary")
+                v-icon.mr-2 mdi-send
+                input#button(
+                  type="submit"
+                  value="Send Email")
+  v-dialog(v-model="successDialogMail")
+    v-card
+      v-img(
+        min-width="70vw"
+        height="50vh"
+        :src="imageURL[1].url")
+        v-card-title.foregroundCard
+          h2.display-1.mx-auto.secondary--text Sent sucessfully!
 </template>
 <script
   type="text/javascript"
@@ -96,11 +92,23 @@ export default {
     message: { required, minLength: minLength(5), maxLength: maxLength(2000) }
   },
   data: () => ({
-    isMounted: false,
     name: "",
     email: "",
     subject: "",
-    message: ""
+    message: "",
+    successDialogMail: false,
+    imageURL: [
+      {
+        title: "contactForm",
+        url:
+          "https://images.unsplash.com/photo-1487611459768-bd414656ea10?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+      },
+      {
+        title: "sucessEmail",
+        url:
+          "https://images.unsplash.com/uploads/141103282695035fa1380/95cdfeef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=3074&q=80"
+      }
+    ]
   }),
   computed: {
     nameErrors() {
@@ -163,7 +171,6 @@ export default {
             emailjs.sendForm(serviceID, templateID, this).then(
               () => {
                 btn.value = "Send Email";
-                alert("Sent!");
               },
               err => {
                 btn.value = "Send Email";
@@ -171,16 +178,47 @@ export default {
               }
             );
           });
+        this.successDialogMail = true;
       } catch (error) {
         console.log(error);
       }
       // this.clear();
     }
-  },
-  mounted() {
-    this.isMounted = true;
   }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#backgroundCard {
+  // Set background image
+  background: border-box center/cover
+    url("https://images.unsplash.com/photo-1487611459768-bd414656ea10?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80");
+  // Add blur
+  filter: blur(5px);
+  -webkit-filter: blur(5px);
+  // Adjust so that it is a background
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-position: center;
+  background-size: cover;
+}
+.foregroundCard {
+  width: 100%;
+  height: 100%;
+}
+#successDialogMail {
+  // Set background image
+  // background: border-box center/cover
+  //   url("https://images.unsplash.com/photo-1586769852044-692d6e3703f0?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=934&q=80");
+  // Add blur
+  filter: blur(5px);
+  -webkit-filter: blur(5px);
+  // Adjust so that it is a background
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-position: center;
+  background-size: cover;
+}
+</style>
