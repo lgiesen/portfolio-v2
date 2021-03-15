4eab9c644db35cabdeb33544c1143db178374135
lgiesen <leo.richard.giesen@gmail.com>
2021-03-15 19:31:18 +0100

    :bug: fix about parallax

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,8 +2,8 @@
 div
   //- Hero Area
   section
-    v-parallax.hero(dark height="100vh"
-      src='https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80')
+    v-parallax.hero(dark
+      src='https://images.unsplash.com/photo-1588392382834-a891154bca4d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1355&q=80')
       v-row(align="center" justify="center")
         v-col.text-center(cols='12')
           h1.display-1.font-weight-thin.mb-4
