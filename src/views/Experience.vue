72f86fdf15579c21fc93875e73a074fccb9a870b
lgiesen <leo.richard.giesen@gmail.com>
2021-03-18 11:50:20 +0100

    :sparkles: SVG rectangle background

--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -36,10 +36,11 @@ div
       sectionSubtitle="I have gained a large variety of work experience and in-depth knowledge"
       imgSrc="https://images.unsplash.com/photo-1581291518857-4e27b48ff24e?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80")
   
-  //- CV: work experience
-  CardGroup(sectionTitle="Education" :iterableObject="employment")
-  
-  CardGroup(sectionTitle="Education" :iterableObject="education")
+  v-img(src="@/assets/background/rectangle.svg")
+    //- CV: work experience
+    CardGroup(sectionTitle="Education" :iterableObject="employment")
+    //- Education
+    CardGroup(sectionTitle="Education" :iterableObject="education")
 
 </template>
 
