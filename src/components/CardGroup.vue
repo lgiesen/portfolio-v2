a6a3071cbe72d8a4abe613dce83dedcc9aebb1f8
lgiesen <leo.richard.giesen@gmail.com>
2021-03-19 20:15:06 +0100

    :art: improve card alignment (mobile and tablet optimization)

--- src/components/CardGroup.vue
+++ src/components/CardGroup.vue
@@ -1,24 +1,23 @@
 <template lang="pug">
 div
   h1.display-2.font-weight-thin.mb-12.text-center {{ this.sectionTitle }}
-  v-container.d-flex.flex-wrap.justify-space-between
-    v-hover(v-for="item in iterableObject" :key="item.title")
-      template(v-slot:default="{ hover }")
-        v-card.mb-12.pa-7.transition-swing(
-          :width="$vuetify.breakpoint.mobile ? '100vw' : '33vw'"
-          :class="`elevation-${hover ? 24 : 6}`")
-          //- if desktop use two columns (38vw)
-          //- else use full width (100vw)
-          v-row
-            v-col(align="center" justify="center")
-              v-img(:src="item.imageSrc" height="10vh" max-width="20vw" contain)
-          v-row
-            v-col
-              v-card-title.justify-center {{ item.title }} 
-              v-card-subtitle.text-center @
-                a(:href="item.link" target="_blank"
-                  ) {{ item.institution }}, {{ item.location }}
-              v-card-text.mt-7 {{ item.description }}
+  v-container
+    v-row(justify="center")
+      v-col.col-12.col-md-8.col-lg-6(v-for="item in iterableObject" :key="item.title")
+        v-hover
+          template(v-slot:default="{ hover }")
+            v-card.mb-12.pa-7.transition-swing(height="100%"
+              :class="`elevation-${hover ? 24 : 6}`")
+              v-row
+                v-col(align="center" justify="center")
+                  v-img(:src="item.imageSrc" height="10vh" max-width="20vw" contain)
+              v-row
+                v-col
+                  v-card-title.justify-center {{ item.title }} 
+                  v-card-subtitle.text-center @
+                    a(:href="item.link" target="_blank"
+                      ) {{ item.institution }}, {{ item.location }}
+                  v-card-text.mt-7 {{ item.description }}
 </template>
 
 <script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -40,18 +40,18 @@ div
   section#scroll
     v-container
       h2.display-2.font-weight-thin.text-center.mb-12 Title  
-      div.d-flex.flex-wrap.justify-space-between
-        v-card.mb-9(v-for="i in 9" :key="i" width="33vw")
-          v-card-title Card {{ i }}
-          v-card-text Magna do deserunt Lorem nulla deserunt. Deserunt mollit minim excepteur 
-            | do consequat eiusmod adipisicing. Fugiat qui adipisicing est occaecat consequat 
-            | nisi est. Laboris ipsum irure enim nulla aute culpa. Voluptate consequat do 
-            | voluptate elit amet.
+      v-row.d-flex.flex-wrap.justify-space-between
+        v-col.col-12.col-sm-6(v-for="i in 9" :key="i")
+          v-card.mb-9
+            v-card-title Card {{ i }}
+            v-card-text Magna do deserunt Lorem nulla deserunt. Deserunt mollit minim excepteur 
+              | do consequat eiusmod adipisicing. Fugiat qui adipisicing est occaecat consequat 
+              | nisi est. Laboris ipsum irure enim nulla aute culpa. Voluptate consequat do 
+              | voluptate elit amet.
   
   section.d-flex.flex-wrap.justify-space-between
     v-img(v-for="(image, index) in associations" :key="index" 
-      :src="associations[index]" max-height="10vh" width="15vw" contain
-      )
+      :src="associations[index]" max-height="10vh" width="15vw" contain)
 </template>
 
 <script>
