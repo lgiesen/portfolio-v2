3c91d750dad31b1048e08386ff5d3353fc4f5552
lgiesen <leo.richard.giesen@gmail.com>
2021-03-19 15:06:13 +0100

    :sparkles: hover cards

--- src/components/CardGroup.vue
+++ src/components/CardGroup.vue
@@ -2,20 +2,23 @@
 div
   h1.display-2.font-weight-thin.mb-12.text-center {{ this.sectionTitle }}
   v-container.d-flex.flex-wrap.justify-space-between
-    v-card.mb-12.pa-7.cardElem(elevation="6" v-for="item in iterableObject" :key="item.title"
-      :width="$vuetify.breakpoint.mobile ? '100vw' : '33vw'")
-      //- if desktop use two columns (38vw)
-      //- else use full width (100vw)
-      v-row
-        v-col(align="center" justify="center")
-          v-img(:src="item.imageSrc" height="10vh" max-width="20vw" contain)
-      v-row
-        v-col
-          v-card-title.justify-center {{ item.title }} 
-          v-card-subtitle.text-center @
-            a(:href="item.link" target="_blank"
-              ) {{ item.institution }}, {{ item.location }}
-          v-card-text.mt-7 {{ item.description }}
+    v-hover(v-for="item in iterableObject" :key="item.title")
+      template(v-slot:default="{ hover }")
+        v-card.mb-12.pa-7.transition-swing(
+          :width="$vuetify.breakpoint.mobile ? '100vw' : '33vw'"
+          :class="`elevation-${hover ? 24 : 6}`")
+          //- if desktop use two columns (38vw)
+          //- else use full width (100vw)
+          v-row
+            v-col(align="center" justify="center")
+              v-img(:src="item.imageSrc" height="10vh" max-width="20vw" contain)
+          v-row
+            v-col
+              v-card-title.justify-center {{ item.title }} 
+              v-card-subtitle.text-center @
+                a(:href="item.link" target="_blank"
+                  ) {{ item.institution }}, {{ item.location }}
+              v-card-text.mt-7 {{ item.description }}
 </template>
 
 <script>
@@ -25,8 +28,4 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-v-card.cardElem:hover {
-  elavation: 25;
-}
-</style>
+<style lang="scss" scoped></style>
