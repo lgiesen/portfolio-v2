db6c46aa29596ec2f8828475c84c7e4b25af6617
lgiesen <leo.richard.giesen@gmail.com>
2021-04-02 21:50:53 +0200

    :sparkles: playing around with link animations

---
+++ src/components/playground/hyperlinks/BackgroundHighlightLink.vue
@@ -0,0 +1,57 @@
+<template lang="pug">
+div.wrap
+  span 
+    a(:href="this.href" :target="this.target") {{ linkText }}
+</template>
+
+<script>
+export default {
+  name: "BackgroundHighlightLink",
+  props: ["href", "linkText", "target"]
+};
+</script>
+
+<style lang="scss" scoped>
+// $link: var(--v-primary-base);
+$link: #f4511e;
+.wrap {
+  //   width: 50vw;
+  //   box-sizing: border-box;
+  //   position: absolute;
+  //   top: 50%;
+  //   left: 50%;
+  transform: translate(0%, 0%);
+  @media screen and (max-width: 800px) {
+    width: 75vw;
+  }
+  span {
+    margin: 0 0 20px;
+    &:last-child {
+      margin: 0;
+    }
+    a {
+      position: relative;
+      overflow: hidden;
+      text-decoration: none !important;
+      color: $link;
+      &::after {
+        content: "";
+        background: rgba($link, 0.25);
+        position: absolute;
+        left: 12px;
+        bottom: -6px;
+        width: calc(100% - 8px);
+        height: calc(100% - 8px);
+        z-index: -1;
+        transition: 0.35s cubic-bezier(0.25, 0.1, 0, 2.05);
+      }
+      &:hover:after {
+        left: 0;
+        bottom: -2px;
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+}
+</style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,58 +1,31 @@
 <template lang="pug">
-v-container.text-center
-  section
+v-container
+  section.text-center
     h1.display-3.font-weight-thin Projects
   section 
     h1.display-3.font-weight-thin Playground
     v-card.pa-10
       v-card-title Special Link animation
-      p This is a paragraph - 
-        a.link(href='#') 
-          span(data-content='ipsum') ipsum
-        |  etc.
-      p Component:
-        AnimatedLink(href="#" linkText="Prop")
+      v-card-text
+        p This is a paragraph - 
+          a.link(href='#') 
+            span(data-content='ipsum') ipsum
+          |  etc.
+        p Component: 
+          AnimatedLink(href="#" linkText="Prop")
+        p Ressource: https://codepen.io/markmead/pen/drORWG:
+          span This is a link with 
+            BackgroundHighlightLink(href="#" linkText="background highlight" target="_self")
+            | .
 </template>
 
 <script>
 import AnimatedLink from "@/components/playground/hyperlinks/AnimatedLink.vue";
+import BackgroundHighlightLink from "@/components/playground/hyperlinks/BackgroundHighlightLink.vue";
 export default {
   name: "Projects",
-  components: { AnimatedLink }
+  components: { AnimatedLink, BackgroundHighlightLink }
 };
 </script>
 
-<style lang="scss" scoped>
-a:not(.gradient-btn) {
-  text-decoration: none !important;
-  cursor: pointer;
-  position: relative;
-  transition: clip-path 275ms ease;
-  &:hover span::before,
-  &:focus span::before {
-    clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
-  }
-}
-.link {
-  position: relative;
-  transition: clip-path 275ms ease;
-  &:hover span::before,
-  &:focus span::before {
-    clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
-  }
-  span {
-    position: relative;
-    display: inline-block;
-    // color: $blue;
-    &::before {
-      position: absolute;
-      content: attr(data-content);
-      // color: $darkblue;
-      text-decoration: underline;
-      // text-decoration-color: $darkblue;
-      clip-path: polygon(0 0, 0 0, 0% 100%, 0 100%);
-      transition: clip-path 275ms ease;
-    }
-  }
-}
-</style>
+<style lang="scss" scoped></style>
