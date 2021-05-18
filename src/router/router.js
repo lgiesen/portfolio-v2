316476afb36d42fdf7726ca889a47cc1968f487d
lgiesen <leo.richard.giesen@gmail.com>
2021-05-18 11:46:17 +0200

    :bug: navigation bug still in place

--- src/router/router.js
+++ src/router/router.js
@@ -74,14 +74,14 @@ const routes = [
 
   // 404-page needs to be at the end so that the wildcard
   // (*) does not get matched with any other URLs
-  {
-    path: "/*",
-    name: "Not Found",
-    component: () =>
-      import(
-        /* webpackChunkName: "404NotFound" */ "../views/errors/404NotFound.vue"
-      )
-  },
+  // {
+  //   path: "/*",
+  //   name: "Not Found",
+  //   component: () =>
+  //     import(
+  //       /* webpackChunkName: "404NotFound" */ "../views/errors/404NotFound.vue"
+  //     )
+  // },
   {
     path: "*",
     redirect: "/"
