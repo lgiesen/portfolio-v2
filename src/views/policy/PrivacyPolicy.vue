9b45605b51a70f130f58cf253427efe3598e711c
lgiesen <leo.richard.giesen@gmail.com>
2021-12-25 19:29:23 +0100

    :pencil2: Update Privacy Policy: Word Break for Mobile Version

--- src/views/policy/PrivacyPolicy.vue
+++ src/views/policy/PrivacyPolicy.vue
@@ -3,6 +3,7 @@ v-container
     h3.text-center Privacy Policy
     p No user data is collected.
     p There are links to LinkedIn, Xing, GitHub, GitLab and Stackoverflow. 
+    p Basic data is recorded with Google Analytics for the sole purpose of optimizing the website.
     p This website was created out of personal interest and seeking of knowledge and its application. 
 </template>
 
