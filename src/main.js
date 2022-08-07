51cbf782f84d7d3239f78024852b2d48b53cbcba
lgiesen <leo.richard.giesen@gmail.com>
2022-08-07 16:33:21 +0200

    :sparkles: init blog

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from "vue";
-// videosection not used at the moment
+// video section not used at the moment
 // import VueVideoSection from "vue-video-section";
 // import "vue-video-section/dist/vue-video-section.css";
 import App from "./App.vue";
@@ -7,9 +7,6 @@ import i18n from "./i18n";
 import vuetify from "./plugins/vuetify";
 import router from "./router/router.js";
 import store from "./store/store.js";
-
-// Vue.component("vue-video-section", VueVideoSection);
-Vue.config.productionTip = false;
 
 new Vue({
   router,
--- src/views/blog/BlogHome.vue
+++ src/views/blog/BlogHome.vue
@@ -9,11 +9,33 @@ div
       :posterSource="require('@/assets/background/nature.jpg')")
       v-container(slot="overlay-content")
         v-row.text-center.pa-12(align="center" justify="center" fill-height fluid)
-          v-col.white--text(cols="12")
+          v-col.white--tepxt(cols="12")
             h1.display-2.mb-4.font-weight-thin {{ $t("blogHero.title") }}
             h1.subtitle-1 {{ $t("blogHero.subtitle") }}
             
     ScrollHero
+  
+  
+  //- Code from about
+  //- Hero Area
+  //- Parallax(:parallaxImage="require('@/assets/about/Aasee.jpg')" 
+  //-   :title="$t('parallaxText.title')" :subtitle="$t('parallaxText.subtitle')")
+  v-img.align-center(src="@/assets/background/Glatthorn.jpg" height="100vh"
+    gradient="to bottom right, rgba(100,115,201,.33), rgba(38,145,66,.5)")
+    //- Another section is necessary to provide the max-width for ultra-wide screens
+    section.mb-0 
+      v-container
+        v-row.pa-md-12.d-flex.text-center
+          v-col.col-12.col-md-6.white--text(order-md="2")
+            h1.display-3.font-weight-thin.mb-12 {{ $t("parallaxText.title") }}
+            p.subheading {{ $t("parallaxText.subtitle") }}
+            ScrollHero.mt-16.pt-16
+          v-col.d-flex.justify-center.col-12.col-md-6(order-md="1")
+            v-card.pa-0(elevation="50" max-width="400px" max-height="400px")
+              v-img(alt="Leo Giesen Profil" src="@/assets/about/Snow-Profile.jpg" max-width="90vw")
+  //- About me texts alternating with corresponding images
+  article
+
   section
     h2.display-2.font-weight-thin.text-center.mb-12 This page is in development 
   section.d-flex.flex-wrap.justify-space-between#scroll
@@ -25,30 +47,38 @@ div
             v-card-title Card {{ i }}
             v-card-text Vue template im Internet recherchieren für Blogposts und 
               | folder structure und referenzieren von Posts (z.B. Übersicht davon)
+  section
+    iframe(style='position:fixed;top:0;left:0;width:100%;height:100%;border:none;margin:0;padding:0;overflow:hidden;z-index:10;' src='https://pseudocode.deepjain.com/')
+    iframe(src="https://v1.embednotion.com/embed/0c7acb7e6cb543d0b6b27fe4fbbd6a40")
+    //- NotionRenderer(:blockMap="blockMap" fullPage)
+    //- vue-notion-render(:unofficial="true" :data="data")
+    //- src="https://www.notion.so/giesen/Productivity-Programs-d37875b3bdad42baaea754dd2100c693" 
+    //- title="Great Applications to Boost Your Productivity"
 </template>
 
 <script>
 import ScrollHero from "@/components/scroll/ScrollHero.vue";
+// import { NotionRenderer, getPageBlocks } from "vue-notion";
 
 export default {
   name: "BlogHome",
   components: {
     ScrollHero
   },
-  data: () => ({}),
+  // data: () => ({ }),
   computed: {},
   i18n: {
     messages: {
-      en: {
-        blogHero: {
-          title: "Blog (catchy name)",
-          subtitle: "lorem"
+      de: {
+        parallaxText: {
+          title: "Blog",
+          subtitle: "Der Blog ist noch in der Entwicklung."
         }
       },
-      de: {
-        blogHero: {
+      en: {
+        parallaxText: {
           title: "Blog",
-          subtitle: "ipsum"
+          subtitle: "The blog is in development."
         }
       }
     }
