95334b294b845bc56abaf61e1b1df8343493938a
Leo Giesen <contact@leogiesen.de>
2024-03-14 12:32:13 +0100

    :sparkles: add scroll to section based on url

--- src/router/router.js
+++ src/router/router.js
@@ -8,7 +8,7 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
   },
   {
     path: "/about",
@@ -17,31 +17,31 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
   {
     path: "/experience",
     name: "Experience",
     component: () =>
-      import(/* webpackChunkName: "experience" */ "../views/Experience.vue")
+      import(/* webpackChunkName: "experience" */ "../views/Experience.vue"),
   },
   {
     path: "/projects",
     name: "Projects",
     component: () =>
-      import(/* webpackChunkName: "projects" */ "../views/Projects.vue")
+      import(/* webpackChunkName: "projects" */ "../views/Projects.vue"),
   },
   {
     path: "/links",
     name: "Links",
     component: () =>
-      import(/* webpackChunkName: "links" */ "../views/Links.vue")
+      import(/* webpackChunkName: "links" */ "../views/Links.vue"),
   },
   {
     path: "/sandbox",
     name: "Sandbox",
     component: () =>
-      import(/* webpackChunkName: "sandbox" */ "../views/Sandbox.vue")
+      import(/* webpackChunkName: "sandbox" */ "../views/Sandbox.vue"),
   },
   // {
   //   path: "/contact",
@@ -56,13 +56,13 @@ const routes = [
     component: () =>
       import(
         /* webpackChunkName: "privacyPolicy" */ "../views/policy/PrivacyPolicy.vue"
-      )
+      ),
   },
   {
     path: "/Imprint",
     name: "Imprint",
     component: () =>
-      import(/* webpackChunkName: "imprint" */ "../views/policy/Imprint.vue")
+      import(/* webpackChunkName: "imprint" */ "../views/policy/Imprint.vue"),
   },
 
   // 404-page needs to be at the end so that the wildcard
@@ -73,15 +73,33 @@ const routes = [
     component: () =>
       import(
         /* webpackChunkName: "404NotFound" */ "../views/errors/404NotFound.vue"
-      )
-  }
+      ),
+  },
 ];
 
 const router = new VueRouter({
   // linkExactActiveClass: "v-tab--active",
   // mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
+  // scroll to element based on URL
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (document.querySelector(to.hash)) {
+            resolve({ selector: to.hash, behavior: "smooth" });
+          } else {
+            resolve({ x: 0, y: 0 });
+          }
+        }, 500); // Delay to ensure that the element is present
+      });
+    } else if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { x: 0, y: 0 };
+    }
+  },
 });
 
 export default router;
