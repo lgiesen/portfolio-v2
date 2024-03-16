37f6bb6403cc2eafff47e14b53fe7c1a1a6d455a
Leo Giesen <contact@leogiesen.de>
2024-03-16 12:44:25 +0100

    :pencil2: update text

--- src/views/policy/PrivacyPolicy.vue
+++ src/views/policy/PrivacyPolicy.vue
@@ -1,10 +1,9 @@
 <template lang="pug">
 v-container
-    h3.text-center Privacy Policy
-    p No user data is collected.
-    p There are links to LinkedIn, Xing, GitHub, GitLab and Stackoverflow. 
-    p Basic data is recorded with Google Analytics for the sole purpose of optimizing the website.
-    p This website was created out of personal interest and seeking of knowledge and its application. 
+  h3.text-center Privacy Policy
+  p There are links to LinkedIn, GitHub, GitLab and StackOverflow. 
+  p Basic data is recorded with Google Analytics for the sole purpose of optimizing the website text.
+  p This website was created out of personal interest and seeking of knowledge and its application. 
 </template>
 
 <script>
