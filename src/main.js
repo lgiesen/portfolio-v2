5efa964dd4f2117b23cac367afb21c2721853f49
Leo Giesen <contact@leogiesen.de>
2024-03-18 22:18:08 +0100

    :fire: rm dead code

--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
 import Vue from "vue";
-// videosection not used at the moment
-// import VueVideoSection from "vue-video-section";
-// import "vue-video-section/dist/vue-video-section.css";
 import App from "./App.vue";
 import i18n from "./i18n";
 import vuetify from "./plugins/vuetify";
 import router from "./router/router.js";
 import store from "./store/store.js";
 
-// Vue.component("vue-video-section", VueVideoSection);
 Vue.config.productionTip = false;
 
 new Vue({
@@ -16,5 +12,5 @@ new Vue({
   vuetify,
   store,
   i18n,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
