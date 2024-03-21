2d62a5b3242ce78c18f386b6b4aaf66320d93921
Leo Giesen <contact@leogiesen.de>
2024-03-21 10:09:03 +0100

    🔧 improve img size

--- src/components/Parallax.vue
+++ src/components/Parallax.vue
@@ -16,9 +16,8 @@ section.mb-0
                   v-html="$t('parallaxText.' + translationKey + '.subtitle')")
                 ScrollHero.mt-16.pt-16
               v-col.center-items.col-12.col-md-6(v-if="smallImgPath" order-md="1")
-                v-card.pa-0(elevation="50" max-width="500" max-height="700")
-                  v-img(:alt="imgAlt" :src="smallImgPath" 
-                    height="100%" max-width="90vw")
+                v-img(:alt="imgAlt" :src="smallImgPath" 
+                  height="500" width="100%" contain)
 </template>
 
 <script>
